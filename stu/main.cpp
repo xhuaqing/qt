89b1b1732e70352ea4c09b6c9db56e20da067829
xhuaqing <xhuaqing@gmail.com>
2013-05-28 17:40:01 +0800

    1111

---
+++ stu/main.cpp
@@ -0,0 +1,45 @@
+#include <QApplication>
+#include <QLabel>
+#include <QPushButton>
+#include <QWidget>
+#include <QSpinBox>
+#include <QSlider>
+#include <QHBoxLayout>
+#include <QString>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+//    QLabel *label = new QLabel("<center><h1><i>Hello, </i><font color=red>Qt!</font></h1></center>");
+//    label->show();
+
+//    QPushButton *button = new QPushButton("Quit");
+//    QObject::connect(button,&QPushButton::clicked,&QApplication::quit);
+//    button->show();
+    QWidget *window = new QWidget;
+    window->setWindowTitle("输入年龄");
+
+    QSpinBox *spinbox = new QSpinBox;
+    QSlider *slider = new QSlider(Qt::Horizontal);
+    spinbox->setRange(0,120);
+    slider->setRange(0,120);
+
+    QObject::connect(spinbox,static_cast<void(QSpinBox:: *)(int)>(&QSpinBox::valueChanged),slider,&QSlider::setValue);
+    QObject::connect(slider,&QSlider::valueChanged,spinbox,&QSpinBox::setValue);
+    QObject::connect(slider,&QSlider::valueChanged,[&](int i){
+        QString str;
+        str.setNum(i);
+        str  = "年龄是：" + str;
+        window->setWindowTitle(str);
+    });
+
+    QHBoxLayout *layout = new QHBoxLayout;
+    layout->addWidget(spinbox);
+    layout->addWidget(slider);
+
+    window->setLayout(layout);
+    window->show();
+
+
+    return a.exec();
+}
---
+++ stu/stu.pro
@@ -0,0 +1,19 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2013-05-28T16:05:11
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = stu
+TEMPLATE = app
+
+
+SOURCES += main.cpp
+
+HEADERS  +=
+
+CONFIG += c++11
